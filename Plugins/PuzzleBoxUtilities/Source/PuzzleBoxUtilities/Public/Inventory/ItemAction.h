e0436b6af042df8255597e0d998ffa3a66b971d5
David Jones <90158160+DavidJones011@users.noreply.github.com>
2024-09-21 15:40:57 -0600

    Started Inventory and Interaction system skeletons.

---
+++ Plugins/PuzzleBoxUtilities/Source/PuzzleBoxUtilities/Public/Inventory/ItemAction.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemAction.generated.h"
+
+//USTRUCT()
+//struct InventoryItem
+//{
+//	GENERATED_BODY()
+//};
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEBOXUTILITIES_API UItemAction : public UObject
+{
+	GENERATED_BODY()
+	
+};
